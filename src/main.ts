b956dcfc52782746b11bb616326dbb7e4e60c90c
Nghiem Pham <nghiemph95@gmail.com>
2023-04-04 20:12:15 +0700

    updaate

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import http from 'http';
-import _ from 'lodash';
-import 'v8-compile-cache';
-import ENV from './config/env';
-import app from './config/express';
-import { getServerHostName } from './config/helper';
-import logger from './config/logger';
-import { database } from './modules/knex';
+import http from "http";
+import _ from "lodash";
+import "v8-compile-cache";
+import ENV from "./config/env";
+import app from "./config/express";
+import { getServerHostName } from "./config/helper";
+import logger from "./config/logger";
+import { database } from "./modules/knex";
 
 export const server = http.createServer(app);
 
 database
-  .raw('select 1+1 as result')
-  .then(() => console.log('connected'))
+  .raw("select 1+1 as result")
+  .then(() => console.log("connected"))
   .catch((err) => {
     console.log(err);
   });
@@ -20,8 +20,8 @@ database
  * start server
  * */
 server.listen(ENV.SERVER.PORT);
-server.on('listening', onListening);
-server.on('error', onError);
+server.on("listening", onListening);
+server.on("error", onError);
 
 /* ============================================================================================================================ */
 /**
@@ -33,14 +33,16 @@ function onListening() {
 }
 
 function onError(error: { syscall: string; code: string }) {
-  if (error.syscall !== 'listen') throw error;
-  const bind = _.isString(ENV.SERVER.PORT) ? `Pipe ${ENV.SERVER.PORT}` : `Port ${ENV.SERVER.PORT}`;
+  if (error.syscall !== "listen") throw error;
+  const bind = _.isString(ENV.SERVER.PORT)
+    ? `Pipe ${ENV.SERVER.PORT}`
+    : `Port ${ENV.SERVER.PORT}`;
 
   /** handle specific listen errors with friendly messages */
   switch (error.code) {
-    case 'EACCES':
+    case "EACCES":
       throw new Error(`${bind} requires elevated privileges`);
-    case 'EADDRINUSE':
+    case "EADDRINUSE":
       throw new Error(`${bind} is already in use`);
     default:
       throw error;
